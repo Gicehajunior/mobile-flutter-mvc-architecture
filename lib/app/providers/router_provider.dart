507a25af9256c52c0a7b5672d016c9a1ba09be10
Gicehajunior <gicehajunior76@gmail.com>
2025-10-10 21:14:20 +0300

    chore: update app name path ref on imports

--- lib/app/providers/router_provider.dart
+++ lib/app/providers/router_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mvcflutterfeatures/routes/web_routes.dart';
+import 'package:mvcflutter/features/routes/web_routes.dart';
 
 final routerProvider = Provider((ref) {
   return appRouter.build(initialLocation: '/');
