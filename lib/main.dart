6e932f53b69fcbe2c604e1d6d7d211ab1e661fbf
Gicehajunior <gicehajunior76@gmail.com>
2025-11-24 13:06:21 +0300

    feat, chore: add and bind forgot pass repo func & update sdk version num

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mvcflutter/app/providers/router_provider.dart';
 import 'public/index.dart';
 
 void main() {
--- lib/public/repos/mobile/auth_repository.dart
+++ lib/public/repos/mobile/auth_repository.dart
@@ -4,4 +4,8 @@ class  AuthRepository extends Repository {
     Future<void> authLogin({required String email, required String password}) async {
         print("Login in Progress..."); 
     }
+
+    Future<void> resetPassword({required String email}) async {
+        print("Reset password in progress...");
+    }
 }
