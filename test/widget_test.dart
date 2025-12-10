f17d6111eebf37c9ba325d2a10a37479114878a4
Giceha Junior <gicehajunior76@gmail.com>
2025-12-10 14:17:45 +0300

    fix: rm unused import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mvcflutter/public/index.dart';
-import 'package:mvcflutter/main.dart';
+import 'package:mvcflutter/public/index.dart'; 
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
