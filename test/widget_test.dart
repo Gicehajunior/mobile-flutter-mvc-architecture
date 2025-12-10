0a5a318f9dfc9a18942478fbb65b151d7ebd33e2
Giceha Junior <gicehajunior76@gmail.com>
2025-12-10 14:12:39 +0300

    fix: include extra required imports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:mvcflutter/public/index.dart';
 import 'package:mvcflutter/main.dart';
 
 void main() {
