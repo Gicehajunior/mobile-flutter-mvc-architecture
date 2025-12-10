bc82176d387895d7ad8fcbb36bff85efe00b7e65
Giceha Junior <gicehajunior76@gmail.com>
2025-12-10 15:19:58 +0300

    chore: rm unused import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,8 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart'; 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';  
 import 'package:mvcflutter/public/index.dart'; 
 
 void main() {
