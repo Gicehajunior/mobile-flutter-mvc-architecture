e0dbf33bc97d37d9b5e40a7a94993e36648f7b00
Giceha Junior <gicehajunior76@gmail.com>
2026-03-23 23:30:22 +0300

    chore: import request onto routing engine

--- lib/routes/app.dart
+++ lib/routes/app.dart
@@ -1,8 +1,9 @@
 import 'package:mvcflutter/config/app_router.dart'; 
+import 'package:mvcflutter/config/app_request.dart';
 import 'package:mvcflutter/app/controllers/mobile/home_controller.dart';
 import 'package:mvcflutter/app/controllers/auth/auth_controller.dart';
 
 final appRouter = AppRouter()
-  ..route('/', 'home', (state) => HomeController().homeView()) 
-  ..route('/login', 'login', (state) => AuthController().loginView())
-  ..route('/forgot-password', 'forgotPassword', (state) => AuthController().forgotPassView());
+  ..route('/', 'home', (request) => HomeController().homeView()) 
+  ..route('/login', 'login', (request) => AuthController().loginView())
+  ..route('/forgot-password', 'forgotPassword', (request) => AuthController().forgotPassView());
