ec1846adbb3c0c4b49860f5b7b124bba76e70b81
Giceha Junior <gicehajunior76@gmail.com>
2026-03-24 23:54:33 +0300

    feat: implement home controller logic and update app routes

--- lib/routes/app.dart
+++ lib/routes/app.dart
@@ -4,6 +4,6 @@ import 'package:mvcflutter/app/controllers/mobile/home_controller.dart';
 import 'package:mvcflutter/app/controllers/auth/auth_controller.dart';
 
 final appRouter = AppRouter()
-  ..route('/', 'home', (request) => HomeController().homeView()) 
+  ..route('/', 'home', (request) => HomeController().homeView(request)) 
   ..route('/login', 'login', (request) => AuthController().loginView())
   ..route('/forgot-password', 'forgotPassword', (request) => AuthController().forgotPassView());
