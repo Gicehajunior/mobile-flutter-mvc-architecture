76471e5711a34d39590bd510646301048275354d
Giceha Junior <gicehajunior76@gmail.com>
2026-03-24 23:55:32 +0300

    chore: update core app configuration and provider registry

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mvcflutter/public/index.dart'; 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
