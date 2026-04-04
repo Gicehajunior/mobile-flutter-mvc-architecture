02a869c688604bf64852786464804d8cf49112cd
Giceha Junior <gicehajunior76@gmail.com>
2026-04-04 13:20:41 +0300

    feat: stabilize & update provider registry & update util functions

--- lib/public/repos/global/repository.dart
+++ lib/public/repos/global/repository.dart
@@ -42,56 +42,78 @@ class Repository {
     );
   }
 
-  Future<void> toggleSubmitBtn(
-      {required void Function(String label) updateLabel,
-      required String currentBtnText,
-      String temporaryLabel = "Processing...",
-      required Future<void> Function() callbackFunc}) async {
-    final original = currentBtnText;
-    updateLabel(temporaryLabel);
-
-    // delay executing the callback for 1 second.
-    await Future.delayed(Duration(seconds: 1));
-
-    try {
-      await callbackFunc();
-    } finally {
-      updateLabel(original);
-    }
+  Future<void> alert({
+    required String status,
+    required String title,
+    required String message,
+    String buttonText = 'Ok',
+    bool showBtn = true,
+    bool isLoading = false,
+    VoidCallback? onConfirm,
+  }) async {
+    return showDialog(
+      context: context,
+      barrierDismissible: !isLoading,
+      builder: (ctx) {
+        return AlertDialog(
+          title: Text(title, textAlign: TextAlign.center),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (isLoading) ...[
+                const LinearProgressIndicator(),
+                const SizedBox(height: 20),
+              ],
+              Text(
+                message,
+                textAlign: TextAlign.center,
+                style: const TextStyle(height: 1.5),
+              ),
+            ],
+          ),
+          actions: [
+            if (showBtn)
+              Center(
+                child: TextButton(
+                  child: Text(buttonText),
+                  onPressed: () {
+                    if (onConfirm != null) {
+                      onConfirm();
+                    }
+                  },
+                ),
+              ),
+          ]);
+      });
   }
 
-  Future<void> alert(
-      {required String status,
-      required String title,
-      required String message,
-      String buttonText = 'Ok'}) async {
-    final dialog = showDialog(
-        context: context,
-        builder: (ctx) {
-          return AlertDialog(
-              title: Text(title),
-              content: Text(message),
-              actions: [
-                // add a button
-                TextButton(
-                    child: Text(buttonText),
-                    onPressed: () {
-                      context.pop();
-                    }),
-              ]);
-        });
-
-    return dialog;
-  }
-
-  Future<void> logout(ProviderRegistry registry) async {
+  Future<void> logout() async { 
     final sm = SessionManager();
+    await sm.clearAll(); // Wait until the physical files are deleted
+    
+    registry.addStateProvider<bool>('isLoggingOutProvider', false);
+    await wait(time: 500, type: 'milliseconds');
 
-    await sm.clearAll();
+    final sessionProvider = registry.getStateProvider('sessionProvider');
+    if (sessionProvider != null) {
+      ref.invalidate(sessionProvider); 
+    }
 
-    // Check ONLY auth token
-    final token = await sm.getSession(key: SessionManager.tokenKey);   
+    registry.updateStateProvider(ref, 'session', Map<String, dynamic>.from({})); 
+    registry.invalidateAllProviders(ref);
+    
+    alert(
+      showBtn: false,
+      isLoading: true,
+      status: 'info', 
+      title: 'Logging Out', 
+      message: 'Please wait as we try logging you out!',
+    );
+          
+    await wait(time: 3, type: 'seconds');
 
+    // Check ONLY auth token 
+    final token = await sm.getSession(key: SessionManager.tokenKey);  
     if (token != null && token.isNotEmpty) {
       alert(
         status: 'error',
@@ -101,10 +123,11 @@ class Repository {
       return;
     }
 
-    // clear provider cache
-    registry.updateStateProvider(ref, 'session', null); 
-
     if (!context.mounted) return;
+
+    registry.updateStateProvider<bool>(ref, 'isLoggingOutProvider', true); 
+    await wait(time: 300, type: 'milliseconds');
+
     context.go('/login');
   }
 }
